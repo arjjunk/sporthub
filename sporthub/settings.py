265be3cf00f0627198cc7f9cb1e2697f3744c1b7
Arjun Gangadhar <101978015+arjungangadhar@users.noreply.github.com>
2023-06-08 20:45:07 +0530

    Update settings.py

--- sporthub/settings.py
+++ sporthub/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'db_sporthub',
-        'USER':'arjun',
-        'PASSWORD':'password',
+        'USER':'root',
+        'PASSWORD':'',
         'HOST':'localhost'
     }
 }
